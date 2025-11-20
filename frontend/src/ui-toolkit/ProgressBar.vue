c38112a87a876199afa4c88f57406ec81af6a745
Nikodem Cyrzan <85363474+NikodemCyrzan@users.noreply.github.com>
2025-11-21 00:26:08 +0100

    feat: add progress bar component to ui-toolkit

---
+++ frontend/src/ui-toolkit/ProgressBar.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+defineProps({
+    value: Number,
+    maxValue: Number,
+});
+
+const startColor = '#2A7B9B';
+const endColor = '#EDDD53';
+</script>
+
+<template>
+    <div class="container">
+        <slot name="left" class="content" />
+        <div class="progress-container">
+            <div
+                class="progress-value"
+                :style="{
+                    width: `${((value || 0) / (maxValue || 1)) * 100}%`,
+                    background: `linear-gradient(90deg, ${startColor} 0%, ${endColor} 100%)`,
+                }"
+            ></div>
+        </div>
+        <slot name="right" class="content" />
+    </div>
+</template>
+
+<style lang="css" scoped>
+.container {
+    position: relative;
+    display: flex;
+    align-items: center;
+    gap: 17px;
+    width: 100%;
+}
+
+.progress-container {
+    overflow: hidden;
+    flex-grow: 1;
+    border-radius: 2px;
+    height: 6px;
+    position: relative;
+    background-color: rgba(0, 0, 0, 0.2);
+}
+
+.progress-value {
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+}
+</style>
---
+++ frontend/src/ui-toolkit/__tests__/ProgressBar.spec.ts
@@ -0,0 +1,29 @@
+import { describe, it, expect } from 'vitest';
+
+import { mount } from '@vue/test-utils';
+import ProgressBar from '../ProgressBar.vue';
+
+describe('HelloWorld', () => {
+    it('renders properly', () => {
+        const wrapper = mount(ProgressBar, {
+            props: { value: 50, maxValue: 100 },
+        });
+        expect(wrapper.isVisible()).toBeTruthy();
+    });
+
+    it('renders left slot', () => {
+        const wrapper = mount(ProgressBar, {
+            props: { value: 50, maxValue: 100 },
+            slots: { left: 'left slot' },
+        });
+        expect(wrapper.text()).toContain('left slot');
+    });
+
+    it('renders right slot', () => {
+        const wrapper = mount(ProgressBar, {
+            props: { value: 50, maxValue: 100 },
+            slots: { left: 'right slot' },
+        });
+        expect(wrapper.text()).toContain('right slot');
+    });
+});
