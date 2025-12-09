8ccd6aeee4dd9aeb219fbda567553a80c7e5e615
Nikodem Cyrzan <85363474+NikodemCyrzan@users.noreply.github.com>
2025-12-09 20:29:11 +0100

    add: uni dialog component to layout

---
+++ frontend/src/layout/UniDialog/UniDialog.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="dialog-background">
+        <div class="dialog-container">
+            <slot />
+        </div>
+    </div>
+</template>
+
+<style lang="css" scoped>
+.dialog-background {
+    background-color: #00000059;
+    backdrop-filter: blur(10px);
+    position: fixed;
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.dialog-container {
+    border-radius: 4px;
+    background-color: #00000040;
+    border: 1px solid var(--color-second-gray);
+    backdrop-filter: blur(10px);
+}
+</style>
--- frontend/src/layout/index.ts
+++ frontend/src/layout/index.ts
@@ -1,5 +1,6 @@
 import MainHeader from './MainHeader/MainHeader.vue';
 import UniSidebar from './UniSidebar/UniSidebar.vue';
 import MainView from './MainView/MainView.vue';
+import UniDialog from './UniDialog/UniDialog.vue';
 
-export { MainHeader, UniSidebar, MainView };
+export { MainHeader, UniSidebar, MainView, UniDialog };
