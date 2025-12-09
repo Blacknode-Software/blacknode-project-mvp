b61adf662ee887a13647704c92ff3fd0c434bb8e
Nikodem Cyrzan <85363474+NikodemCyrzan@users.noreply.github.com>
2025-12-09 19:24:53 +0100

    add: view structure

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -4,4 +4,14 @@ import { MainHeader } from '@/layout';
 
 <template>
     <MainHeader :sections="['DEPARTMENT OF SECURITY', 'SECTION II']" />
+    <div class="view-sections-container"></div>
 </template>
+
+<style scoped>
+.view-sections-container {
+    flex-direction: row;
+    display: flex;
+    flex-grow: 1;
+    background-color: transparent;
+}
+</style>
