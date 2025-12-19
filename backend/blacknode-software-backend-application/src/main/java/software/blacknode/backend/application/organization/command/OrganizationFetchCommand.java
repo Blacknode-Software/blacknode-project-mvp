892c7a813e4bcd218dd9e8558114fdbdd80e5fb6
scraft official <63715927+scraft-official@users.noreply.github.com>
2025-12-19 22:15:35 +0100

    Add organization fetch command and use case
    Introduces OrganizationFetchCommand and OrganizationFetchUseCase to enable fetching organization details with access control checks. The use case retrieves the organization for the current session and ensures the member has read access.

---
+++ backend/blacknode-software-backend-application/src/main/java/software/blacknode/backend/application/organization/command/OrganizationFetchCommand.java
@@ -0,0 +1,10 @@
+package software.blacknode.backend.application.organization.command;
+
+import lombok.Getter;
+import me.hinsinger.hinz.common.huid.HUID;
+import software.blacknode.backend.application.command.ExecutionCommand;
+
+@Getter
+public class OrganizationFetchCommand implements ExecutionCommand {
+	
+}
