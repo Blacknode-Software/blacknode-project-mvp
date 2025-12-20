4b32b89e6fd4643be82d82dba8773260eaf855d2
Nikodem Cyrzan <85363474+NikodemCyrzan@users.noreply.github.com>
2025-12-20 09:52:55 +0100

    feat: add expandable channels

--- frontend/src/components/MainSidebar/ProjectsList/ExpandableProject.vue
+++ frontend/src/components/MainSidebar/ProjectsList/ExpandableProject.vue
@@ -1,24 +1,69 @@
 <script setup lang="ts">
 import type { Project } from '@/shared-types/project';
+import { ref } from 'vue';
 
-defineProps<{ project: Project }>();
+const props = defineProps<{ project: Project; forceExpand?: boolean }>();
+
+const isExpanded = ref(props.forceExpand);
+
+const handleExpand = () => {
+    isExpanded.value = !isExpanded.value;
+    console.log(isExpanded.value);
+};
 </script>
 
 <template>
-    <li class="project-select">{{ project.name }}</li>
+    <li>
+        <div class="project-button" :onclick="handleExpand">
+            {{ project.name }}
+        </div>
+        <ul :class="[isExpanded! ? 'project-drawer-closed' : '', 'project-drawer']">
+            <li v-for="channel in project.channels" :key="channel.uuid">
+                <div class="channel-button">
+                    {{ channel.name }}
+                </div>
+            </li>
+        </ul>
+    </li>
 </template>
 
 <style scoped>
-.project-select {
+.project-button {
     padding: 3px 8px;
+    cursor: pointer;
     color: white;
     font-size: 16px;
-    cursor: pointer;
     border-radius: 6px;
     user-select: none;
 }
 
-.project-select:hover {
+.project-button:hover {
+    background-color: #00000021;
+}
+
+.project-drawer {
+    interpolate-size: allow-keywords;
+    list-style-type: none;
+    padding: 0;
+    padding-left: 10px;
+    overflow: hidden;
+    transition: height 100ms ease-in-out;
+}
+
+.project-drawer-closed {
+    height: 0px;
+}
+
+.channel-button {
+    padding: 3px 8px;
+    cursor: pointer;
+    color: white;
+    font-size: 16px;
+    border-radius: 6px;
+    user-select: none;
+}
+
+.channel-button:hover {
     background-color: #00000021;
 }
 </style>
