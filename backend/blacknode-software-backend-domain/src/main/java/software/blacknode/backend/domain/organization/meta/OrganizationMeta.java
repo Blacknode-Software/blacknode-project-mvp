4911442a12d381a2e2e887798a174adf0e9c1f61
scraft official <63715927+scraft-official@users.noreply.github.com>
2025-12-20 17:50:20 +0100

    Move organization name to OrganizationMeta
    Refactored Organization to store its name in OrganizationMeta instead of directly in Organization. Added validation and builder support for the name field in OrganizationMeta. Updated creation and modification logic to use OrganizationMeta for name management.

--- backend/blacknode-software-backend-domain/src/main/java/software/blacknode/backend/domain/organization/meta/OrganizationMeta.java
+++ backend/blacknode-software-backend-domain/src/main/java/software/blacknode/backend/domain/organization/meta/OrganizationMeta.java
@@ -1,5 +1,20 @@
 package software.blacknode.backend.domain.organization.meta;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.With;
+
+
+@Getter
+@With
+@Builder
 public class OrganizationMeta {
+	
+	@NotBlank
+	@Size(min = 3)
+	@Builder.Default
+	private final String name = "Unnamed Organization";
 
 }
