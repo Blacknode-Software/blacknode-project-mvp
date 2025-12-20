6d340d412a110b45040318f9142ba725743278be
scraft official <63715927+scraft-official@users.noreply.github.com>
2025-12-20 22:49:06 +0100

    Refactor organization controller endpoints
    Updated OrganizationController to use dependency injection, new mappers, and use cases for fetching and patching organization details. Simplified endpoint paths, removed unused methods, and added support for mapping requests and responses. Added builder pattern to OrganizationFetchCommand.

--- backend/blacknode-software-backend-api/src/main/java/software/blacknode/backend/api/controller/organization/OrganizationController.java
+++ backend/blacknode-software-backend-api/src/main/java/software/blacknode/backend/api/controller/organization/OrganizationController.java
@@ -1,50 +1,77 @@
 package software.blacknode.backend.api.controller.organization;
 
-import java.util.List;
-import java.util.UUID;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import software.blacknode.backend.api.controller.BaseController;
+import software.blacknode.backend.api.controller.organization.annotation.OrganizationHeader;
+import software.blacknode.backend.api.controller.organization.mapper.impl.OrganizationFetchMapper;
+import software.blacknode.backend.api.controller.organization.mapper.impl.OrganizationPatchMapper;
 import software.blacknode.backend.api.controller.organization.request.OrganizationPatchRequest;
+import software.blacknode.backend.api.controller.organization.response.OrganizationPatchResponse;
 import software.blacknode.backend.api.controller.organization.response.OrganizationResponse;
+import software.blacknode.backend.application.organization.command.OrganizationFetchCommand;
+import software.blacknode.backend.application.organization.usecase.OrganizationFetchUseCase;
+import software.blacknode.backend.application.organization.usecase.OrganizationPatchUseCase;
 
-@Hidden
 @RestController
+@RequiredArgsConstructor
 @Tag(name = "Organizations", description = "Organization management APIs")
 public class OrganizationController extends BaseController {
 
-	@Operation(summary = "Get an organization by ID")
-	@ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Found the organization"),
+	private final OrganizationFetchMapper organizationFetchMapper;
+	private final OrganizationFetchUseCase organizationFetchUseCase;
+	
+	private final OrganizationPatchMapper organizationPatchMapper;
+	private final OrganizationPatchUseCase organizationPatchUseCase;
+	
+	@OrganizationHeader
+	@Operation(summary = "Get organization details")
+	@ApiResponses(value = { 
+			@ApiResponse(responseCode = "200", description = "Organization details"),
 			@ApiResponse(responseCode = "404", description = "Organization not found") })
-	@GetMapping("/organizations/{organizationId}")
-	public ResponseEntity<OrganizationResponse> getOrganization(@PathVariable UUID id) {
-		return ResponseEntity.ok(null);
+	@GetMapping("/organization}")
+	public ResponseEntity<OrganizationResponse> getOrganization() {
+		var command = OrganizationFetchCommand.builder().build();
+		
+		var result = organizationFetchUseCase.execute(command);
+		
+		var response = organizationFetchMapper.toResponse(result);
+		
+		return response.toOkResponse("Organization fetched successfully!");
 	}
 	
-	@Operation(summary = "Get all organizations")
-	@ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Found organizations") })
-	@GetMapping("/organizations")
-	public ResponseEntity<List<OrganizationResponse>> getOrganizations() {
-		return ResponseEntity.ok(null);
-	}
+//	@Operation(summary = "Get all organizations")
+//	@ApiResponses(value = { 
+//			@ApiResponse(responseCode = "200", description = "Found organizations") })
+//	@GetMapping("/organizations")
+//	public ResponseEntity<List<OrganizationResponse>> getOrganizations() {
+//		return ResponseEntity.ok(null);
+//	}
 	
-	@Operation(summary = "Update an existing organization")
-	@ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Organization updated"),
+	@OrganizationHeader
+	@Operation(summary = "Update organization")
+	@ApiResponses(value = { 
+			@ApiResponse(responseCode = "200", description = "Organization updated"),
+			@ApiResponse(responseCode = "400", description = "Invalid request"),
 			@ApiResponse(responseCode = "404", description = "Organization not found") })
-	@PatchMapping("/organizations/{organizationId}")
-	public ResponseEntity<OrganizationResponse> patchOrganization(@PathVariable UUID id, @RequestBody OrganizationPatchRequest request) {
-		return ResponseEntity.ok(null);
+	@PatchMapping("/organization")
+	public ResponseEntity<OrganizationPatchResponse> patchOrganization(@RequestBody OrganizationPatchRequest request) {
+		var command = organizationPatchMapper.toCommand(request);
+		
+		var result = organizationPatchUseCase.execute(command);
+		
+		var response = organizationPatchMapper.toResponse(result);
+		
+		return response.toOkResponse("Organization updated successfully!");
 	}
 	
 }
