2625bffa197334eaf5aa1495655d1814f66284d4
scraft official <63715927+scraft-official@users.noreply.github.com>
2025-12-26 16:08:39 +0100

    Refactor Task entity to use meta objects
    Introduces TaskMeta, TaskCreationMeta, TaskModificationMeta, and TaskDeletionMeta abstractions for task metadata. Refactors Task to use these meta objects, replacing direct field access with encapsulated metadata and updating creation, modification, and deletion logic. Adds builder and implementation classes for meta objects to support flexible task property management.

---
+++ backend/blacknode-software-backend-domain/src/main/java/software/blacknode/backend/domain/task/meta/modify/impl/TaskEndAtTimestampModificationMeta.java
@@ -0,0 +1,20 @@
+package software.blacknode.backend.domain.task.meta.modify.impl;
+
+import lombok.Builder;
+import lombok.ToString;
+import me.hinsinger.hinz.common.time.timestamp.Timestamp;
+import software.blacknode.backend.domain.task.meta.modify.TaskModificationMeta;
+
+import java.util.Optional;
+
+@Builder
+@ToString
+public class TaskEndAtTimestampModificationMeta implements TaskModificationMeta {
+
+    private final Timestamp endAt;
+
+    public Optional<Timestamp> getEndAtTimestamp() {
+        return Optional.ofNullable(endAt);
+    }
+
+}
