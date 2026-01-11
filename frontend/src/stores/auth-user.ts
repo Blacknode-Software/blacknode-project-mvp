475b0083f7fa09a87339802c9a2770425eaa6451
Nikodem Cyrzan <85363474+NikodemCyrzan@users.noreply.github.com>
2026-01-11 14:10:45 +0100

    feat: add authentication api service and storage

--- frontend/src/stores/auth-user.ts
+++ frontend/src/stores/auth-user.ts
@@ -1,14 +1,26 @@
+import { useAuthenticationApiService } from '@/api-services/authentication';
+import { UnixTimestamp } from '@/utils';
 import { defineStore } from 'pinia';
+import { ref } from 'vue';
 
 export const useAuthUserStore = defineStore('authUser', () => {
-    async function refreshTasks(/* channelId: string */) {
-        // example:
-        //
-        // const res = await channelsApiService.requestAllChannelsForProject({
-        //     organizationId: channelId,
-        //     projectId: channelId,
-        // });
+    const apiService = useAuthenticationApiService();
+    const isAuthenticated = ref(false);
+    const errorMessage = ref<string | undefined>();
+    const accessToken = ref<string | undefined>();
+    const accessTokenExpiresAt = ref<string | UnixTimestamp>();
+
+    async function requestAuthentication(email: string, password: string) {
+        const res = await apiService.requestAuthentication({ email, password });
+
+        if (res.isOk()) {
+            accessToken.value = res.value.accessToken;
+            accessTokenExpiresAt.value = new UnixTimestamp(res.value.accessTokenExpiresAt);
+            isAuthenticated.value = true;
+        } else {
+            errorMessage.value = 'Something went wrong';
+        }
     }
 
-    return { refreshTasks };
+    return { requestAuthentication };
 });
