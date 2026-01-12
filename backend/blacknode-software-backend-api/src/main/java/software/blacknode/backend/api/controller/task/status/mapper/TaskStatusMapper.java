c9a7fdf71b32e1776a465746f923758e1801b6e8
scraft official <63715927+scraft-official@users.noreply.github.com>
2026-01-12 22:49:13 +0100

    Refactor date fields to use Long timestamps
    Changed Instant fields to Long in InviteResponseContent and TaskResponseContent for easier serialization. Added mapping methods between Timestamp and Long in BaseMapper. Annotated TaskStatusMapper with @Mapper for MapStruct integration.

--- backend/blacknode-software-backend-api/src/main/java/software/blacknode/backend/api/controller/task/status/mapper/TaskStatusMapper.java
+++ backend/blacknode-software-backend-api/src/main/java/software/blacknode/backend/api/controller/task/status/mapper/TaskStatusMapper.java
@@ -1,10 +1,13 @@
 package software.blacknode.backend.api.controller.task.status.mapper;
 
+import org.mapstruct.Mapper;
+
 import software.blacknode.backend.api.controller.mapper.ControllerMapper;
 import software.blacknode.backend.api.controller.task.status.response.content.TaskStatusResponseContent;
 import software.blacknode.backend.api.controller.task.status.response.content.annotation.TaskStatusResponseContentMapping;
 import software.blacknode.backend.domain.task.status.TaskStatus;
 
+@Mapper(componentModel = "spring")
 public interface TaskStatusMapper extends ControllerMapper {
 
 	@TaskStatusResponseContentMapping
